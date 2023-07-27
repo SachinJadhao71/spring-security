2e2f54b144054867a198601e0c78e5056a7acf46
sachin jadhao <jadhaosachin7171@gmail.com>
2023-07-27 11:51:20 +0530

    role based authentication and password set

---
+++ src/main/java/com/example/springsecurity/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/welcome")
+    public String studentArea(){
+        return "Welcome To The STUDENT Area";
+    }
+}
